3f07e96b2eabffd28643f5d84407432642deff8a
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-03 15:34:24 +0200

    calcolo sconto età

--- assets/main.js
+++ assets/main.js
@@ -11,14 +11,27 @@ btnGenera.addEventListener("click" , function(){
     var nomePassegero = document.getElementById("nome").value;
         console.log("Nome del passeggero:", nomePassegero);
 
-        var distanza = document.getElementById("kilometri").value;
+        var distanza = document.getElementById("distanza").value;
         console.log("Km da percorrere:", distanza);
 
-        var età = document.getElementById("select_età").value;
+        var età = document.getElementById("età").value;
         console.log("Età:", età);
 
-        var prezzo = Math.floor(km * 0.21);
-        var discount;
-        var discountType;
+        var prezzo = Math.floor(distanza * 0.21);
+        var sconto;
+        var tipoOfferta;
 
+        // calcolo sconto per età
+        if (età == "minorenne") {
+            sconto = (prezzo * 0.2).toFixed(2); 
+            tipoOfferta = "Offerta Minorenni";
+        } else if (età =="over65") {
+            sconto = (prezzo * 0.4).toFixed(2);
+            tipoOfferta = "Offerta Over 65";
+        } else {
+            sconto = 0;
+            tipoOfferta = "Nessuna offerta";
+        }
+        var prezzoFinale=(prezzo - sconto).toFixed(2);
+        console.log(prezzoFinale);
 })
